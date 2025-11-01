22b26887aca8cf13231b77d62db2a2907a4805f5
Kaidstor <kaidstor@gmail.com>
2025-11-01 10:43:31 +0500

    feat: настроен билд всех страниц

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 import tailwindcss from "@tailwindcss/vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [tailwindcss()],
@@ -7,6 +13,16 @@ export default defineConfig({
     watch: {
       // следить за всеми html в корне
       ignored: [],
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        book: resolve(__dirname, "book.html"),
+        campsites: resolve(__dirname, "campsites.html"),
+        rvSites: resolve(__dirname, "rv-sites.html"),
+      },
     },
   },
 });
